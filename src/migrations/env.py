d2a2cb7f53287dcd370bc2f4ea8ad50b5a47e8b8
Jeressy <trixjessie@gmail.com>
2024-11-10 19:22:59 -0300

    Add K8s file

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -10,7 +10,7 @@ from alembic import context
 config = context.config
 
 # Interpret the config file for Python logging.
-# This line sets up loggers basically.
+# This line sets up loggers basically..
 fileConfig(config.config_file_name)
 logger = logging.getLogger('alembic.env')
 
